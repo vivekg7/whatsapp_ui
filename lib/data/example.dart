8deaead3087d203f31bd4eebf8ef65328c2fa829
Vivek Kumar Gupta <vivekkumargupta680@gmail.com>
2020-02-02 07:47:50 +0530

    Added Profile examples

---
+++ lib/data/example.dart
@@ -0,0 +1,10 @@
+import 'package:whatsapp_ui/model/profile.dart';
+
+class Example {
+  final Profile p1 = Profile("Harry Potter", "alive", "harry.jpg", "7777777");
+  final Profile p2 = Profile("Ronald Weasley", "alive", "ron.jpg", "8888888");
+  final Profile p3 = Profile("Hermione Granger", "alive", "hermione.jpg", "3333333");
+  final Profile p4 = Profile("Draco Malfoy", "alive", "draco.jpg", "1111111");
+  final Profile p5 = Profile("Sirius Black", "alive", "sirius.jpg", "9999999");
+  final Profile self = Profile("ttttttt", "ggggggg", "harry.jpg", "848484848");
+}
---
+++ lib/model/profile.dart
@@ -0,0 +1,9 @@
+class Profile {
+  String name;
+  String status;
+  String dplink;
+  String mobile;
+  String lastSeen;
+
+  Profile(this.name, this.status, this.dplink, this.mobile, {this.lastSeen});
+}
