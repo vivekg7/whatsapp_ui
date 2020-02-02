2f0d99877f3c84468472e68beae96b7c08eff108
Vivek Kumar Gupta <vivekkumargupta680@gmail.com>
2020-02-02 08:01:53 +0530

    Added Color Util

---
+++ lib/util/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class ColorUtil {
+  Color themeDark = Color.fromARGB(255, 0, 255, 0);
+  Color themeLight = Color.fromARGB(255, 0, 255, 0);
+  Color bg = Color.fromARGB(255, 255, 255, 255);
+  Color chatPageBg = Color.fromARGB(255, 255, 255, 255);
+  Color chatSelfBg = Color.fromARGB(255, 0, 255, 0);
+  Color chatOtherBg = Color.fromARGB(255, 255, 255, 255);
+  Color tileBg = Color.fromARGB(255, 0, 255, 0);
+  Color headText = Color.fromARGB(255, 255, 255, 255);
+  Color text = Color.fromARGB(255, 0, 0, 0);
+}
